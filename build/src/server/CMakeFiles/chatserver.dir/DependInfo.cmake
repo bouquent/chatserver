6fd2e67c9c46b3605e051e942be7db66e3c031d6
czz <1870058972@qq.com>
2021-10-23 17:08:04 +0800

    my second commit

---
+++ build/src/server/CMakeFiles/chatserver.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xxs/Desktop/chat/chatserver/src/server/chatserver.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/chatserver.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/chatservice.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/chatservice.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/db/mysql.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/db/mysql.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/main.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/main.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/model/friendmodel.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/model/friendmodel.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/model/groupmodel.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/model/groupmodel.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/model/offlinemessagemodel.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/model/offlinemessagemodel.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/model/usermodel.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/model/usermodel.cpp.o"
+  "/home/xxs/Desktop/chat/chatserver/src/server/redis/redis.cpp" "/home/xxs/Desktop/chat/chatserver/build/src/server/CMakeFiles/chatserver.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../thirdparty"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/redis"
+  "../include/server/model"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
